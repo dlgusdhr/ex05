0c5caeb88b0451b13f2f0fe4161de0c20d7c25a5
dlgusdhr <dlgusdhr0501@naver.com>
2023-04-01 11:35:43 +0900

    20230401

--- src/components/PostsPage.jsx
+++ src/components/PostsPage.jsx
@@ -12,7 +12,7 @@ const PostsPage = () => {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json())
             .then(json => {
-                setLast(Math.ceil(json.length / 5));
+                setLast(Math.ceil(json.length / 20));
                 let start = (page - 1) * 5 + 1;
                 let end = page * 5;
                 setPosts(json.filter(post => post.id >= start && post.id <= end));
